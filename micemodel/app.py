006268d085ff206542c2ece8ee1f5c62b3aedd41
Sara Mohamed <sara_mohamed@aucegypt.edu>
2024-03-02 11:57:04 +0200

    changes

---
+++ micemodel/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, render_template,  send_file
+from mice import Model  # Assuming model.py is in the same directory
+
+app = Flask(__name__)
+model = Model()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/generate', methods=['POST'])
+def generate_object_file():
+    # look at test.py for the input data
+    data = request.get_json()
+
+    weight = data['weight']
+    height = data['height']
+    hips = data['hips']
+    chest = data['chest']
+    waist = data['waist']
+    gender = data['gender'] 
+
+    model.generate_object_file(weight, height, hips, chest,waist ,gender)
+
+    return send_file("/home/g02-f23/Desktop/Application/Thesis-Flutter-Frontend/assets/meshes/result.obj")
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000, debug=True)
